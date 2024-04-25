b9ccf649b726ae6ed011b6d8e9c8abe1bd16328e
heeneeee <gmldnjd0614@gmail.com>
2024-04-26 00:49:01 +0900

    config : webSocket server 연결

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -9,4 +11,14 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+// 이렇게 하면 http 서버와 wss 서버를 둘 다 돌릴 수 있음
+// 동일한 포트에서 두 개 다 처리 가능
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function handleConnection() {}
+
+wss.on("connection", handleConnection);
+
+server.listen(3000, handleListen);
